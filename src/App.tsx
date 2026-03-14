442a70627662c34aca4488555def0e9ce02ca6dc
Magiri1115 <busmagiri6@gmail.com>
2026-03-14 22:07:48 +0900

    Reactアプリのエントリーポイントとベーススタイルの作成

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>Visual Timer</h1>
+      <p>Setting up the project infrastructure...</p>
+    </div>
+  );
+}
+
+export default App;
