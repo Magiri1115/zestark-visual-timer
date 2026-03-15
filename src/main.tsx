13e67c4dd4865dca3627052c4ca5364d71268141
Magiri1115 <busmagiri6@gmail.com>
2026-03-15 18:22:42 +0900

    feat(frontend): Set up main App, global styles, and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/global.css";
 
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-}
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
